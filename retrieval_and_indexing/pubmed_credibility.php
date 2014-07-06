34d4c19b8e053367e61ca39f8e8300eded559fa8
georg <gpetz@users.noreply.github.com>
2014-07-06 16:59:48 +0200

    crediblity of wikipedia and pubmed articles

---
+++ retrieval_and_indexing/pubmed_credibility.php
@@ -0,0 +1,41 @@
+<?php
+
+//http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?retmode=xml&dbfrom=pubmed&id=19755503&cmd=neighbor&linkname=pubmed_pubmed_citedin
+
+echo pubmed_count_citedin('19755503') . " citations";
+
+function pubmed_count_citedin($id) {
+
+    $url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi';
+
+    $params = array(
+        'dbfrom' => 'pubmed',
+        'retmode' => 'xml',
+        'id' => $id,
+        'cmd' => 'neighbor',
+        'linkname' => 'pubmed_pubmed_citedin'
+    );
+
+    foreach ($params as $key => $value) {
+        $params_string .= $key . '=' . $value . '&';
+    }
+    rtrim($params_string, '&');
+
+    //open connection
+    $ch = curl_init();
+
+    // Set the url, number of POST vars, POST data
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, count($params));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    $xml = simplexml_load_string($response);
+
+    $result = $xml->xpath("/eLinkResult/LinkSet/LinkSetDb[LinkName='pubmed_pubmed_citedin' and DbTo='pubmed']/Link/Id");
+
+    curl_close($ch);
+
+    return count($result);
+}
--- retrieval_and_indexing/wikipedia_credibility.php
+++ retrieval_and_indexing/wikipedia_credibility.php
@@ -2,49 +2,61 @@
 
 require_once './simple_html_dom.php';
 
-function filter_urls($url) {
-    if ((substr($url, 0, 4) == "Talk")
-            or ( strpos($url, "action=history") !== false)
-            or ( strpos($url, "%3A") !== false)
-            or ( strpos($url, "List%20of") !== false)) {
-        return false;
-    } else {
-        return true;
-    }
+$article_labels = Array();
+
+$limit = 300;
+
+// Get article labels belonging to 'Medicine' Wikipedia project
+for ($i = 1; $i < 12000; $i += $limit) {
+    $article_labels = array_merge($article_labels, get_toolserver_response($i, "Medicine"));
+    sleep(2);
 }
 
-$project = "Medicine";
-$offset = 2;
-$url = "http://tools.wmflabs.org/enwp10/cgi-bin/list2.fcgi?run=yes&projecta=" . $project .
-        "&namespace=&pagename=&quality=&importance=&score=&limit=2&offset=" . $offset .
-        "&sorta=Importance&sortb=Quality";
-echo $url . "\n";
-$response = file_get_contents($url);
-echo $response."\n";
+//// Get article labels belonging to 'Pharmacology' Wikipedia project
+//for ($i = 1; $i < 10000; $i += $limit) {
+//	$article_labels = array_merge($article_labels, get_toolserver_response($i, "Pharmacology"));
+//	sleep(2);
+//}
 
-$html = file_get_html($url);
-foreach ($html->find('tr') as $element) {
-    //echo $element . "--------------------------\n";
-    preg_match("/\"http\:\/\/en\.wikipedia\.org\/w\/index\.php\?title\=([^\"]+)/", $element, $matches);
-    if (strcmp("", $matches[1]) != 0) {
-        $match = urldecode(str_replace("%20", "_", $matches[1]));
-        echo $match . ", ";
-        //.$element."\n";
-        echo "Importance: " . $element->find('b', 0)->plaintext . ", ";
-        echo "Quality: " . $element->find('b', 1)->plaintext . ", ";
-        echo "Review Release: " . $element->find('b', 2)->plaintext . "\n";
+// Remove duplicates
+$article_labels_unique = array_unique($article_labels);
+
+echo count($article_labels) - count($article_labels_unique) . " dulplicate(s)\n";
+echo count($article_labels_unique) . " total\n";
+
+function get_toolserver_response($offset, $project) {
+    $url = "http://tools.wmflabs.org/enwp10/cgi-bin/list2.fcgi?run=yes&projecta=" . $project .
+            "&namespace=&pagename=&quality=&importance=&score=&limit=" . $GLOBALS["limit"] . "&offset=" . $offset .
+            "&sorta=Importance&sortb=Quality";
+    echo $url . "\n";
+
+    $matches_returned = Array();
+
+    $html = file_get_html($url);
+
+    foreach ($html->find('tr') as $element) {
+        //echo $element . "--------------------------\n";
+        preg_match("/\"http\:\/\/en\.wikipedia\.org\/w\/index\.php\?title\=([^\"]+)/", $element, $matches);
+        if (strcmp("", $matches[1]) != 0) {
+            $match = urldecode(str_replace("%20", "_", $matches[1]));
+//            echo $match . ", ";
+            
+            if (strpos($match, ';') !== false){
+                echo $match . "\n";
+            }
+
+            $quality = $element->find('b', 1)->plaintext;
+
+            //.$element."\n";
+//            echo "Importance: " . $element->find('b', 0)->plaintext . ", ";
+//            echo $quality . "\n";
+//            echo "Review Release: " . $element->find('b', 2)->plaintext . "\n";
+
+            $matches_returned[] = $match . ";" . $quality;
+        }
     }
+
+    return $matches_returned;
 }
 
-preg_match_all("/\"http\:\/\/en\.wikipedia\.org\/w\/index\.php\?title\=([^\"]+)/", $response, $matches);
-$matches = $matches[1];
-$matches = array_filter($matches, "filter_urls");
-
-$matches_returned = Array();
-foreach ($matches as $match) {
-    $matches_returned[] = urldecode(str_replace("%20", "_", $match));
-    foreach ($matches_returned as $match_returned) {
-        echo $match_returned . "\n";
-    }
-}
 ?>
