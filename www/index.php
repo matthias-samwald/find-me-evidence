a8236141fbbeae66f566c7f85a3e6456aa40af3d
Georg Petz <gpetz@users.noreply.github.com>
2014-09-13 21:04:56 +0200

    seems to be a working hack for issue #10

--- www/index.php
+++ www/index.php
@@ -136,10 +136,22 @@ if ($user_query != "") {
                 $ul.html( html );
                 $ul.listview( "refresh" );
                 $ul.trigger( "updatelayout");
-            });            
+            });
+            
+            pubmedAutocomplete();
+            
             $input.focus();
         }	   
 	}
+        
+        function pubmedAutocomplete() {
+            $.getJSON("http://preview.ncbi.nlm.nih.gov/portal/utils/autocomp.fcgi?dict=pm_related_queries_2&callback=?&q="+ encodeURIComponent($("#q").val()), NSuggest_CreateData);
+            return false;
+        }
+            
+        function NSuggest_CreateData(q, data) {
+            ;
+        }
 </script>
 </head>
 <body>
