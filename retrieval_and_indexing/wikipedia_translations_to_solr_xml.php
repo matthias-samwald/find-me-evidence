739d11454ea0f62466ec112eb30ed176c389f6e5
Georg Petz <gpetz@users.noreply.github.com>
2014-11-16 14:18:49 +0100

    mesh translation are now getting added to the solr index

--- retrieval_and_indexing/wikipedia_translations_to_solr_xml.php
+++ retrieval_and_indexing/wikipedia_translations_to_solr_xml.php
@@ -6,22 +6,28 @@ include('./config.php');
 
 $count = 0;
 
-if (($handle = fopen('./wikipedia/es_translated_relevant_articles_credibility.txt', 'r')) !== FALSE) {
-    while (($row = fgetcsv($handle, 1000, ';')) !== FALSE) {
+index("es_translated_relevant_articles_credibility.txt", 2, 3);
 
-        $output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><update><add><doc>\n";
-        $output .= "<field name='id'>" . $row[0] . "</field>\n";
-        $output .= "<field name='title'>" . $row[0] . "</field>\n";
-        $output .= "<field name='german'>" . $row[2] . "</field>\n";
-        $output .= "<field name='spanish'>" . $row[3] . "</field>\n";
-        $output .= "</doc></add></update>";
+//index("new_medical_terms.txt", 1, 2);
 
-        do_post_request(SOLR_URL_DIC . '/update', $output);
-        
-        echo $count++ . "\n";
-        
+function index($translationfile, $german_column, $spanish_column) {
+
+    if (($handle = fopen('./wikipedia/' . $translationfile, 'r')) !== FALSE) {
+        while (($row = fgetcsv($handle, 1000, ';')) !== FALSE) {
+
+            $output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><update><add><doc>\n";
+            $output .= "<field name='id'>" . trim($row[0]) . "</field>\n";
+            $output .= "<field name='title'>" . trim($row[0]) . "</field>\n";
+            $output .= "<field name='german'>" . trim($row[$german_column]) . "</field>\n";
+            $output .= "<field name='spanish'>" . trim($row[$spanish_column]) . "</field>\n";
+            $output .= "</doc></add></update>";
+
+            do_post_request(SOLR_URL_DIC . '/update', $output);
+
+            echo $count++ . "\n";
+        }
+        fclose($handle);
     }
-    fclose($handle);
-}
 
-print do_post_request(SOLR_URL_DIC . '/update', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><update><commit/><optimize/></update>");
+    print do_post_request(SOLR_URL_DIC . '/update', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><update><commit/><optimize/></update>");
+}
