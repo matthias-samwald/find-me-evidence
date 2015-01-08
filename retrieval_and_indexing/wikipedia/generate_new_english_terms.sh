6eda6b810da9a848ec7e4056380a5ec44f4a5b0a
Georg Petz <gpetz@users.noreply.github.com>
2015-01-08 20:43:27 +0100

    switch to relevant_articles_credibility.txt

--- retrieval_and_indexing/wikipedia/generate_new_english_terms.sh
+++ retrieval_and_indexing/wikipedia/generate_new_english_terms.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #generate sorted list of wikipedia terms
-cat relevant_articles.txt | sed 's/_/ /g' | sort > wikipedia_articles_sorted_tmp.txt
+cat relevant_articles_credibility.txt | cut -d';' -f1 | sed 's/_/ /g' | sort > wikipedia_articles_sorted_tmp.txt
 #generate sorted list of mesh terms
 cat mshd2015.txt | tail -n+5 | sort > mesh_descriptors_sorted_tmp.txt
 #generate list of new terms
-comm -23 --check-order mesh_descriptors_sorted_tmp.txt wikipedia_articles_sorted_tmp.txt > new_medical_terms.txt
+comm -23 mesh_descriptors_sorted_tmp.txt wikipedia_articles_sorted_tmp.txt > new_medical_terms.txt
 #delete tmp files
 rm wikipedia_articles_sorted_tmp.txt
 rm mesh_descriptors_sorted_tmp.txt
