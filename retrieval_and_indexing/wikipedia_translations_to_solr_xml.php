98dd591c2df36f1d20fa3b8d1fe11f308ea0fd8a
Georg Petz <gpetz@users.noreply.github.com>
2015-01-17 14:41:59 +0100

    send 1000 docs at once to solr, should fix #24

--- retrieval_and_indexing/wikipedia_translations_to_solr_xml.php
+++ retrieval_and_indexing/wikipedia_translations_to_solr_xml.php
@@ -5,6 +5,7 @@ require_once('./lib/http_post/http_post.php');
 include('./config.php');
 
 $count = 0;
+$output = "";
 
 index("es_de_translated_relevant_articles_credibility.txt", 2, 3);
 
@@ -12,23 +13,41 @@ index("new_medical_terms.txt", 1, 2);
 
 function index($translationfile, $german_column, $spanish_column) {
 
+    global $output, $count;
+    $remainder = -1;
+
     if (($handle = fopen('./wikipedia/' . $translationfile, 'r')) !== FALSE) {
         while (($row = fgetcsv($handle, 1000, ';')) !== FALSE) {
 
-            $output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><update><add><doc>\n";
-            $output .= "<field name='id'>" . htmlspecialchars($row[0]) . "</field>\n";
+            $remainder = ++$count % 1000;
+
+            $output .= "<doc><field name='id'>" . htmlspecialchars($row[0]) . "</field>\n";
             $output .= "<field name='title'>" . htmlspecialchars($row[0]) . "</field>\n";
             $output .= "<field name='german'>" . htmlspecialchars($row[$german_column]) . "</field>\n";
-            $output .= "<field name='spanish'>" . htmlspecialchars($row[$spanish_column]) . "</field>\n";
-            $output .= "</doc></add></update>";
+            $output .= "<field name='spanish'>" . htmlspecialchars($row[$spanish_column]) . "</field></doc>\n";
 
-            echo ++ $GLOBALS["count"]. "\n";
-
-            do_post_request(SOLR_URL_DIC . '/update', $output);
-            
+            if ($remainder == 0) {
+                update($output);
+            }
         }
+        if ($remainder > 0) {
+            update($output);
+        }
+        $count = 0;
         fclose($handle);
     }
 
     print do_post_request(SOLR_URL_DIC . '/update', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><update><commit/><optimize/></update>");
+}
+
+function update($docs) {
+
+    global $output, $count;
+
+    $output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><update><add>\n" .
+            $docs .
+            "</add></update>";
+    do_post_request(SOLR_URL_DIC . '/update', $output);
+    $output = "";
+    echo $count . "\n";
 }
