292ce78680c6330f08cfc283a5f3eb121ba644d5
Georg Petz <gpetz@users.noreply.github.com>
2015-02-22 07:57:33 +0100

    remove everything in brackets for translation purposes, closes #27

--- retrieval_and_indexing/wikipedia_translations_to_solr_xml.php
+++ retrieval_and_indexing/wikipedia_translations_to_solr_xml.php
@@ -20,11 +20,15 @@ function index($translationfile, $german_column, $spanish_column) {
         while (($row = fgetcsv($handle, 1000, ';')) !== FALSE) {
 
             $remainder = ++$count % 1000;
+            
+            $english = removeBracketsAndContent($row[0]);
+            $german = removeBracketsAndContent($row[$german_column]);
+            $spanish = removeBracketsAndContent($row[$spanish_column]);
 
-            $output .= "<doc><field name='id'>" . htmlspecialchars($row[0]) . "</field>\n";
-            $output .= "<field name='title'>" . htmlspecialchars($row[0]) . "</field>\n";
-            $output .= "<field name='german'>" . htmlspecialchars($row[$german_column]) . "</field>\n";
-            $output .= "<field name='spanish'>" . htmlspecialchars($row[$spanish_column]) . "</field></doc>\n";
+            $output .= "<doc><field name='id'>" . htmlspecialchars($english) . "</field>\n";
+            $output .= "<field name='title'>" . htmlspecialchars($english) . "</field>\n";
+            $output .= "<field name='german'>" . htmlspecialchars($german) . "</field>\n";
+            $output .= "<field name='spanish'>" . htmlspecialchars($spanish) . "</field></doc>\n";
 
             if ($remainder == 0) {
                 update($output);
@@ -38,6 +42,10 @@ function index($translationfile, $german_column, $spanish_column) {
     }
 
     print do_post_request(SOLR_URL_DIC . '/update', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><update><commit/><optimize/></update>");
+}
+
+function removeBracketsAndContent($term) {
+    return preg_replace("/\(.*\)/u", "", $term);
 }
 
 function update($docs) {
